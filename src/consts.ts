94ca45de96bbaca34f7df858116cc369a7f39db1
fooiskandar <fooiskandar@gmail.com>
2025-03-22 21:48:58 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Metadata, Site, Socials } from "@types";
 export const SITE: Site = {
   TITLE: "Iskandar PRO MK.",
   DESCRIPTION: "Tentang aku dan Kamu.",
-  EMAIL: "trevortylerlee@gmail.com",
+  EMAIL: "fooiskandar@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
