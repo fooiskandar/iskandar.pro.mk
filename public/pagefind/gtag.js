cb93c32b9684972d4517d0cbfbff890f9c87f1bc
fooiskandar <fooiskandar@gmail.com>
2025-03-26 00:08:21 +0700

    Create gtag.js

---
+++ public/pagefind/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HG5HMVNWFV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HG5HMVNWFV');
+</script>
