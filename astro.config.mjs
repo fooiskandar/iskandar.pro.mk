a019b533b2f77d6b6f4b46560609d779abd2fbf3
fooiskandar <fooiskandar@gmail.com>
2025-03-27 09:55:29 +0700

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
-import partytown from ‘@astrojs/partytown‘;
+// import partytown from ‘@astrojs/partytown‘;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://iskandar.pro.mk",
   integrations: [sitemap(), mdx(), pagefind(),
-  partytown({
+//  partytown({
       // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
+//      config: {
+//        forward: ["dataLayer.push"],
+//      },
+//    }),
   ],
   vite: {
     plugins: [tailwindcss()],
