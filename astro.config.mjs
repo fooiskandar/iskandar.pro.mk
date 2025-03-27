71170cfbddecfe16a2ce2a92f2e13d94ab4137af
fooiskandar <fooiskandar@gmail.com>
2025-03-27 11:39:33 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
-import partytown from ‘@astrojs/partytown‘;
+// import partytown from ‘@astrojs/partytown‘;
 
 // https://astro.build/config
 export default defineConfig({
